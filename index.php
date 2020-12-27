6ec94e3574eb120a656aa272ae48f03dce489149
Harika <h.duyu@rebuy.com>
2020-12-27 11:01:48 +0100

    change logo and update index

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+	include('config/db_connect.php');
+
+	// write query for all pizzas
+	$sql = 'SELECT title, body, date FROM articles ORDER BY date';
+
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$articles = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	function snippet($body){
+		return substr($body, 0, 50) . '...';
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	
+	<?php include('templates/base_layout.php'); ?>
+
+	<h1>Article List</h1>
+  <div class="articles">
+		<?php foreach($articles as $article): ?>
+      <div class="article">
+        <h2><a href="#article-detail"><?php echo $article['title']; ?></a></h2>
+        <p><?php echo snippet($article['body']); ?></p>
+        <p><?php echo $article['date']; ?></p>
+        <p class="author">added by <?php echo 'h.duyu'; /*$article['author_id']*/  ?></p>
+      </div>
+			<?php endforeach; ?>
+  </div>
+<!--footer-->
+  </div>
+</body>
+
+</html>
--- templates/base_layout.php
+++ templates/base_layout.php
@@ -1,27 +1,27 @@
 <?php 
 
-  session_start();
+  // session_start();
 
-  //$_SESSION['name'] = 'mario';
+  // //$_SESSION['name'] = 'mario';
 
-  if($_SERVER['QUERY_STRING'] == 'noname'){
-    //unset($_SESSION['name']);
-    session_unset();
-  }
+  // if($_SERVER['QUERY_STRING'] == 'noname'){
+  //   //unset($_SESSION['name']);
+  //   session_unset();
+  // }
 
-  $name = $_SESSION['name'];
+  // $name = $_SESSION['name'];
 
 ?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Django Blog</title>
+  <title>PHP Blog</title>
   <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
-  <link rel="stylesheet" href="{% static 'styles.css' %}">
+  <link rel="stylesheet" href="/php-blog/templates/styles.css">
 </head>
 <body>
   <header>
-    <h1><a href="#"><img class="logo" src="/img/pizzasvg" alt="django_blog"></a></h1>
+    <h1><a href="/php-blog/"><img class="logo" src="/php-blog/img/logo.png" alt="php_blog"></a></h1>
     <nav>
       <ul>
         <li>
